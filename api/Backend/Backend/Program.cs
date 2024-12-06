5b279e09a02a1f979b2bf8b52bf4a3b4b621cdd3
stijn-castelyns <stijn@u2u.be>
2024-12-06 15:49:52 +0100

    Add backend

---
+++ api/Backend/Backend/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+  private static void Main(string[] args)
+  {
+    var builder = FunctionsApplication.CreateBuilder(args);
+
+    builder.ConfigureFunctionsWebApplication();
+
+    // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
+    // builder.Services
+    //     .AddApplicationInsightsTelemetryWorkerService()
+    //     .ConfigureFunctionsApplicationInsights();
+
+    builder.Build().Run();
+  }
+}
